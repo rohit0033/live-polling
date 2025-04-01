d78e907fb21d9116290964352833c43bbf134925
rohit0033 <rohusharma2003@gmail.com>
2025-04-01 23:49:33 +0530

    Add more options button

--- src/pages/TeacherDashboard.tsx
+++ src/pages/TeacherDashboard.tsx
@@ -596,7 +596,35 @@ const TeacherDashboard: React.FC = () => {
                           </div>
                         ))}
                       </div>
+                      {options.length < 6 && (
+                        <Button
+                          type="button"
+                          variant="outline"
+                          size="sm"
+                          onClick={addOption}
+                          className="mt-3 text-poll-primary border-poll-primary hover:bg-poll-light/30"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="mr-1"
+                          >
+                            <line x1="12" y1="5" x2="12" y2="19"></line>
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                          </svg>
+                          Add More Option
+                        </Button>
+                      )}
+                    
                     </div>
+                    
 
                     <div>
                       <label className="block text-sm font-medium mb-1">
